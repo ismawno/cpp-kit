0b65410b6b665211a6358aa4d0f8a450f7d8ca72
Ismael Bueno <ismaelwno@gmail.com>
2022-12-08 14:25:36 +0100

    Added a bunch of debug logs

--- include/debug.h
+++ include/debug.h
@@ -9,26 +9,26 @@
     printf("-DBG- LOG in %s at line %d: ", __FILE__, __LINE__); \
     printf(__VA_ARGS__);
 
-#define DBG_ASSERT_LOG(cond,                                                                 \
-                       ...)                                                                  \
-    if (!(cond))                                                                             \
-    {                                                                                        \
-        printf("-DBG- LOG in %s at line %d with condition %s: ", __FILE__, __LINE__, #cond); \
-        printf(__VA_ARGS__);                                                                 \
+#define DBG_LOG_IF(cond,                                                                            \
+                   ...)                                                                             \
+    if (cond)                                                                                       \
+    {                                                                                               \
+        printf("-DBG- ASSERT LOG in %s at line %d with condition %s: ", __FILE__, __LINE__, #cond); \
+        printf(__VA_ARGS__);                                                                        \
     }
 
-#define DBG_ASSERT(cond,                                                                               \
-                   ...)                                                                                \
-    if (!(cond))                                                                                       \
-    {                                                                                                  \
-        fprintf(stderr, "-DBG- EXIT in %s at line %d with condition %s: ", __FILE__, __LINE__, #cond); \
-        fprintf(stderr, __VA_ARGS__);                                                                  \
-        exit(EXIT_FAILURE);                                                                            \
+#define DBG_ASSERT(cond,                                                                                 \
+                   ...)                                                                                  \
+    if (!(cond))                                                                                         \
+    {                                                                                                    \
+        fprintf(stderr, "-DBG- ASSERT in %s at line %d with condition %s: ", __FILE__, __LINE__, #cond); \
+        fprintf(stderr, __VA_ARGS__);                                                                    \
+        exit(EXIT_FAILURE);                                                                              \
     }
 #else
 #define DBG_LOG(...)
-#define DBG_ASSERT_LOG(cond, \
-                       ...)
+#define DBG_LOG_IF(cond, \
+                   ...)
 #define DBG_ASSERT(cond, \
                    ...)
 #endif
