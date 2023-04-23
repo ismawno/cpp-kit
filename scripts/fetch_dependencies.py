2b21d54f5ff68efdf7a5896529e80686820b361a
Ismael Bueno <ismaelwno@gmail.com>
2023-04-23 14:41:25 +0200

    fetch_dependencies.py will fetch glm instead of vec-2D

--- scripts/fetch_dependencies.py
+++ scripts/fetch_dependencies.py
@@ -5,7 +5,11 @@ from typing import Union
 
 
 def add_dependencies(
-    dependencies: Union[str, list[str]], exec: str, branch: str, folder: str = "."
+    dependencies: Union[str, list[str]],
+    exec: str,
+    branch: str,
+    folder: str = ".",
+    user: str = "ismawno",
 ) -> None:
     if isinstance(dependencies, str):
         dependencies = [dependencies]
@@ -19,7 +23,7 @@ def add_dependencies(
                 "git",
                 "submodule",
                 "add",
-                f"https://github.com/ismawno/{dependency}.git",
+                f"https://github.com/{user}/{dependency}.git",
                 f"{folder}/{dependency}",
             ]
         )
