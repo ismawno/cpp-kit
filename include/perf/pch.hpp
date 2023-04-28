7e87f3d174390933a1fe3dbfd9f9851a2932c7cd
Ismael Bueno <ismaelwno@gmail.com>
2023-04-29 01:14:52 +0200

    Added pch files

---
+++ include/perf/pch.hpp
@@ -0,0 +1,14 @@
+#ifndef PERF_PCH_HPP
+#define PERF_PCH_HPP
+
+#include <unordered_map>
+#include <cstdint>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <chrono>
+#include <cmath>
+#include <algorithm>
+#include <filesystem>
+
+#endif
