06af6943143cdf69e647024e61dc2b64b8ab22cc
Ismael Bueno <ismaelwno@gmail.com>
2023-05-14 01:32:04 +0200

    Preallocate and free functions are now inline to prevent linker errors

--- include/mem/stack_allocator.hpp
+++ include/mem/stack_allocator.hpp
@@ -22,8 +22,10 @@ namespace mem
 
     inline std::size_t _stack_size = 0, _entry_index = 0; // Set this atomic??
     inline std::array<stack_entry, MAX_ENTRIES> _stack_entries;
-    inline char *_stack_buffer = new char[STACK_CAPACITY];
-    // void free_stack() { delete[] _stack_buffer; }
+    inline char *_stack_buffer = nullptr;
+
+    inline void preallocate_stack() { _stack_buffer = new char[STACK_CAPACITY]; }
+    inline void free_stack() { delete[] _stack_buffer; }
 
     template <typename T>
     class stack_allocator : public std::allocator<T>
@@ -49,7 +51,7 @@ namespace mem
 
         ptr allocate(size n)
         {
-            DBG_ASSERT_CRITICAL(_entry_index < MAX_ENTRIES - 1, "No more entries available in stack allocator when trying to allocate {0} bytes! Maximum: {1}", n * sizeof(T), MAX_ENTRIES)
+            DBG_ASSERT_CRITICAL(_entry_index < (MAX_ENTRIES - 1), "No more entries available in stack allocator when trying to allocate {0} bytes! Maximum: {1}", n * sizeof(T), MAX_ENTRIES)
 
             const bool enough_space = _stack_size + n * sizeof(T) < STACK_CAPACITY;
             DBG_ASSERT_WARN(enough_space, "No more space available in stack allocator when trying to allocate {0} bytes! Maximum: {1} bytes", STACK_CAPACITY)
@@ -66,12 +68,12 @@ namespace mem
 
         void deallocate(ptr p, size n)
         {
+            _entry_index--;
             DBG_ASSERT_CRITICAL(p == (ptr)_stack_entries[_entry_index].data, "Trying to deallocate disorderly from stack allocator!")
             if (_stack_entries[_entry_index].used_default)
                 base::deallocate(p, n);
             else
                 _stack_size -= n * sizeof(T);
-            _entry_index--;
             DBG_DEBUG("Stack deallocating {0} bytes of data. {1} entries and {2} bytes remaining in buffer.", n * sizeof(T), MAX_ENTRIES - _entry_index, STACK_CAPACITY - _stack_size)
         }
     };
