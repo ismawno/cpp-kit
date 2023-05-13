7757987566d54b0a599eab79692d9d279b0cc04e
Ismael Bueno <ismaelwno@gmail.com>
2023-05-13 20:05:36 +0200

    Logs are now called through spdlog runtime functions

--- include/dbg/log.hpp
+++ include/dbg/log.hpp
@@ -16,38 +16,42 @@
 #define DBG_BREAK() __debugbreak();
 #endif
 
-#define DBG_SET_LEVEL(lvl) spdlog::set_level(lvl);
+#define DBG_SET_LEVEL(lvl) spdlog::set_level(spdlog::level::lvl);
 #define DBG_SET_PATTERN(patt) spdlog::set_pattern(patt);
 
-#define DBG_TRACE(...) SPDLOG_TRACE(__VA_ARGS__);
-#define DBG_INFO(...) SPDLOG_INFO(__VA_ARGS__);
-#define DBG_WARN(...) SPDLOG_WARN(__VA_ARGS__);
-#define DBG_ERROR(...)         \
-    SPDLOG_ERROR(__VA_ARGS__); \
+#define DBG_TRACE(...) spdlog::trace(__VA_ARGS__);
+#define DBG_DEBUG(...) spdlog::debug(__VA_ARGS__);
+#define DBG_INFO(...) spdlog::info(__VA_ARGS__);
+#define DBG_WARN(...) spdlog::warn(__VA_ARGS__);
+#define DBG_ERROR(...)          \
+    spdlog::error(__VA_ARGS__); \
     DBG_BREAK()
-#define DBG_CRITICAL(...)         \
-    SPDLOG_CRITICAL(__VA_ARGS__); \
+#define DBG_CRITICAL(...)          \
+    spdlog::critical(__VA_ARGS__); \
     DBG_BREAK()
 
 #define DBG_ASSERT_TRACE(cond, ...) \
     if (!(cond))                    \
-        SPDLOG_TRACE(__VA_ARGS__);
+        spdlog::trace(__VA_ARGS__);
+#define DBG_ASSERT_DEBUG(cond, ...) \
+    if (!(cond))                    \
+        spdlog::debug(__VA_ARGS__);
 #define DBG_ASSERT_INFO(cond, ...) \
     if (!(cond))                   \
-        SPDLOG_INFO(__VA_ARGS__);
+        spdlog::info(__VA_ARGS__);
 #define DBG_ASSERT_WARN(cond, ...) \
     if (!(cond))                   \
-        SPDLOG_WARN(__VA_ARGS__);
+        spdlog::warn(__VA_ARGS__);
 #define DBG_ASSERT_ERROR(cond, ...) \
     if (!(cond))                    \
     {                               \
-        SPDLOG_ERROR(__VA_ARGS__);  \
+        spdlog::error(__VA_ARGS__); \
         DBG_BREAK()                 \
     }
 #define DBG_ASSERT_CRITICAL(cond, ...) \
     if (!(cond))                       \
     {                                  \
-        SPDLOG_CRITICAL(__VA_ARGS__);  \
+        spdlog::critical(__VA_ARGS__); \
         DBG_BREAK()                    \
     }
 #else
