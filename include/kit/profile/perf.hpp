cc9c8b225bec09557e681c07061b195853650698
Ismael Bueno <ismaelwno@gmail.com>
2023-07-16 01:06:10 +0200

    Added kit prefix to perf macros

--- include/kit/profile/perf.hpp
+++ include/kit/profile/perf.hpp
@@ -5,27 +5,27 @@
 #include "kit/profile/profiler.hpp"
 
 #ifdef KIT_PROFILE
-#define PERF_BEGIN_SESSION(name, pexport) kit::profiler::get().begin_session(pexport, name);
-#define PERF_END_SESSION() kit::profiler::get().end_session();
-#define PERF_SCOPE(name) kit::timer tm##__LINE__(name);
-#define PERF_FUNCTION() kit::timer tm##__LINE__(__FUNCTION__);
+#define KIT_PERF_BEGIN_SESSION(name, pexport) kit::profiler::get().begin_session(pexport, name);
+#define KIT_PERF_END_SESSION() kit::profiler::get().end_session();
+#define KIT_PERF_SCOPE(name) kit::timer tm##__LINE__(name);
+#define KIT_PERF_FUNCTION() kit::timer tm##__LINE__(__FUNCTION__);
 #ifdef __PRETTY_FUNCTION__
-#define PERF_PRETTY_FUNCTION() kit::timer tm##__LINE__(__PRETTY_FUNCTION__);
+#define KIT_PERF_PRETTY_FUNCTION() kit::timer tm##__LINE__(__PRETTY_FUNCTION__);
 #else
-#define PERF_PRETTY_FUNCTION() PERF_FUNCTION()
+#define KIT_PERF_PRETTY_FUNCTION() KIT_PERF_FUNCTION()
 #endif
-#define PERF_SET_MAX_FILE_MB(size) kit::profiler::get().max_mb(size);
-#define PERF_SET_EXTENSION(ext) kit::profiler::get().extension(ext);
-#define PERF_SET_PATH(pth) kit::profiler::get().path(pth);
+#define KIT_PERF_SET_MAX_FILE_MB(size) kit::profiler::get().max_mb(size);
+#define KIT_PERF_SET_EXTENSION(ext) kit::profiler::get().extension(ext);
+#define KIT_PERF_SET_PATH(pth) kit::profiler::get().path(pth);
 #else
-#define PERF_BEGIN_SESSION(pexport, name)
-#define PERF_END_SESSION()
-#define PERF_SCOPE(name)
-#define PERF_FUNCTION()
-#define PERF_PRETTY_FUNCTION()
-#define PERF_SET_MAX_FILE_MB(size)
-#define PERF_SET_EXTENSION(extension)
-#define PERF_SET_PATH(path)
+#define KIT_PERF_BEGIN_SESSION(pexport, name)
+#define KIT_PERF_END_SESSION()
+#define KIT_PERF_SCOPE(name)
+#define KIT_PERF_FUNCTION()
+#define KIT_PERF_PRETTY_FUNCTION()
+#define KIT_PERF_SET_MAX_FILE_MB(size)
+#define KIT_PERF_SET_EXTENSION(extension)
+#define KIT_PERF_SET_PATH(path)
 #endif
 
 #endif
