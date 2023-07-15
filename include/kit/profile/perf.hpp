e3741715b2be4314076ea7ab48edcbe73226348a
Ismael Bueno <ismaelwno@gmail.com>
2023-07-15 20:26:11 +0200

    Removed perf:: namespace

--- include/kit/profile/perf.hpp
+++ include/kit/profile/perf.hpp
@@ -5,18 +5,18 @@
 #include "kit/profile/profiler.hpp"
 
 #ifdef KIT_PERF
-#define PERF_BEGIN_SESSION(name, pexport) perf::profiler::get().begin_session(pexport, name);
-#define PERF_END_SESSION() perf::profiler::get().end_session();
-#define PERF_SCOPE(name) perf::timer tm##__LINE__(name);
-#define PERF_FUNCTION() perf::timer tm##__LINE__(__FUNCTION__);
+#define PERF_BEGIN_SESSION(name, pexport) kit::profiler::get().begin_session(pexport, name);
+#define PERF_END_SESSION() kit::profiler::get().end_session();
+#define PERF_SCOPE(name) kit::timer tm##__LINE__(name);
+#define PERF_FUNCTION() kit::timer tm##__LINE__(__FUNCTION__);
 #ifdef __PRETTY_FUNCTION__
-#define PERF_PRETTY_FUNCTION() perf::timer tm##__LINE__(__PRETTY_FUNCTION__);
+#define PERF_PRETTY_FUNCTION() kit::timer tm##__LINE__(__PRETTY_FUNCTION__);
 #else
 #define PERF_PRETTY_FUNCTION() PERF_FUNCTION()
 #endif
-#define PERF_SET_MAX_FILE_MB(size) perf::profiler::get().max_mb(size);
-#define PERF_SET_EXTENSION(ext) perf::profiler::get().extension(ext);
-#define PERF_SET_PATH(pth) perf::profiler::get().path(pth);
+#define PERF_SET_MAX_FILE_MB(size) kit::profiler::get().max_mb(size);
+#define PERF_SET_EXTENSION(ext) kit::profiler::get().extension(ext);
+#define PERF_SET_PATH(pth) kit::profiler::get().path(pth);
 #else
 #define PERF_BEGIN_SESSION(pexport, name)
 #define PERF_END_SESSION()
