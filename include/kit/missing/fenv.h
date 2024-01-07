d11cc822dd0a22d0837b945b0454f6fc31d78e02
Ismael Bueno <ismaelwno@gmail.com>
2024-01-07 22:58:35 +0100

    Removed windows definitions for feenableexcept as they are already available

--- include/kit/missing/fenv.h
+++ include/kit/missing/fenv.h
@@ -114,28 +114,4 @@ inline int fedisableexcept(unsigned int mask)
 }
 
 #endif
-
-#else
-inline int feenableexcept(unsigned int excepts)
-{
-#pragma STDC FENV_ACCESS ON
-    fexcept_t flags;
-    /* Save current exception flags. */
-    fegetexceptflag(&flags, FE_ALL_EXCEPT);
-
-    feclearexcept(FE_ALL_EXCEPT);                              /* clear all fp exception conditions */
-    return fesetexceptflag(&flags, excepts) != 0 ? -1 : flags; /* set new flags */
-}
-
-inline int fedisableexcept(unsigned int excepts)
-{
-#pragma STDC FENV_ACCESS ON
-    fexcept_t flags;
-    /* Save current exception flags. */
-    fegetexceptflag(&flags, FE_ALL_EXCEPT);
-
-    feclearexcept(FE_ALL_EXCEPT);                               /* clear all fp exception conditions */
-    return fesetexceptflag(&flags, ~excepts) != 0 ? -1 : flags; /* set new flags */
-}
-
 #endif
