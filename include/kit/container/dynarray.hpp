70d9c4c86c7fd9b077f6bc677b849875b2314505
Ismael Bueno <ismaelwno@gmail.com>
2024-02-22 18:31:16 +0100

    Removed is_base_of as it requires the type to be fully defined in some cases. Dynarray uses specific array iterators

--- include/kit/container/dynarray.hpp
+++ include/kit/container/dynarray.hpp
@@ -18,8 +18,8 @@ template <typename T, std::size_t Capacity> class dynarray
     using const_reference = const value_type &;
     using pointer = value_type *;
     using const_pointer = const value_type *;
-    using iterator = pointer;
-    using const_iterator = const_pointer;
+    using iterator = typename std::array<value_type, Capacity>::iterator;
+    using const_iterator = typename std::array<value_type, Capacity>::const_iterator;
     using reverse_iterator = std::reverse_iterator<iterator>;
     using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 
