caf8fdc456bf673ca6aa9b52601d9c2fa95467af
Ismael Bueno <ismaelwno@gmail.com>
2024-03-24 02:04:46 +0000

    Remove current custom allocators: pretty useless

--- include/kit/memory/ref.hpp
+++ include/kit/memory/ref.hpp
@@ -1,32 +1,10 @@
 #pragma once
 
-#ifdef KIT_USE_CUSTOM_ALLOC
-#include "kit/memory/block_allocator.hpp"
-#endif
 #include <memory>
 
 namespace kit
 {
 template <typename T> using ref = std::shared_ptr<T>;
-
-#ifdef KIT_USE_CUSTOM_ALLOC
-
-template <typename T, std::size_t BlockSize = 16 * 1024, std::size_t MaxChunkSize = 1024,
-          std::uint32_t SupportedSizesIncrement = 32, class... Args>
-inline ref<T> make_ref(Args &&...args)
-{
-    if (!block_allocator<BlockSize, MaxChunkSize, SupportedSizesIncrement>::template can_allocate<T>())
-        return std::make_shared<T>(std::forward<Args>(args)...);
-
-    T *buff = block_allocator<BlockSize, MaxChunkSize, SupportedSizesIncrement>::template allocate<T>();
-    T *ptr = new (buff) T(std::forward<Args>(args)...);
-
-    KIT_ASSERT_WARN((std::uint64_t)ptr % alignof(T) == 0, "Block allocated pointer {0} is not aligned! Alignment: {1}",
-                    (void *)ptr, alignof(T))
-    return ref<T>(ptr, block_deleter<T, BlockSize, MaxChunkSize, SupportedSizesIncrement>());
-}
-
-#else
 
 template <typename T, std::size_t BlockSize = 16 * 1024, std::size_t MaxChunkSize = 1024,
           std::uint32_t SupportedSizesIncrement = 32, class... Args>
@@ -34,6 +12,4 @@ inline ref<T> make_ref(Args &&...args)
 {
     return std::make_shared<T>(std::forward<Args>(args)...);
 }
-
-#endif
 } // namespace kit
--- include/kit/memory/scope.hpp
+++ include/kit/memory/scope.hpp
@@ -1,46 +1,13 @@
 #pragma once
 
-#ifdef KIT_USE_CUSTOM_ALLOC
-#include "kit/memory/block_allocator.hpp"
-#endif
 #include <memory>
-#include <cstddef>
-#include <cstdint>
 
 namespace kit
 {
-#ifdef KIT_USE_CUSTOM_ALLOC
-template <typename T, std::size_t BlockSize = 16 * 1024, std::size_t MaxChunkSize = 1024,
-          std::uint32_t SupportedSizesIncrement = 32>
-using scope = std::unique_ptr<T, block_deleter<T, BlockSize, MaxChunkSize, SupportedSizesIncrement>>;
+template <typename T> using scope = std::unique_ptr<T>;
 
-template <typename T, std::size_t BlockSize = 16 * 1024, std::size_t MaxChunkSize = 1024,
-          std::uint32_t SupportedSizesIncrement = 32, class... Args>
-inline scope<T, BlockSize, MaxChunkSize, SupportedSizesIncrement> make_scope(Args &&...args)
-{
-    if (!block_allocator<BlockSize, MaxChunkSize, SupportedSizesIncrement>::template can_allocate<T>())
-        return scope<T, BlockSize, MaxChunkSize, SupportedSizesIncrement>(new T(std::forward<Args>(args)...));
-
-    T *buff = block_allocator<BlockSize, MaxChunkSize, SupportedSizesIncrement>::template allocate<T>();
-    T *ptr = new (buff) T(std::forward<Args>(args)...);
-
-    KIT_ASSERT_WARN((std::uint64_t)ptr % alignof(T) == 0, "Block allocated pointer {0} is not aligned! Alignment: {1}",
-                    (void *)ptr, alignof(T))
-    return scope<T, BlockSize, MaxChunkSize, SupportedSizesIncrement>(ptr);
-}
-
-#else
-
-template <typename T, std::size_t BlockSize = 16 * 1024, std::size_t MaxChunkSize = 1024,
-          std::uint32_t SupportedSizesIncrement = 32>
-using scope = std::unique_ptr<T>;
-
-template <typename T, std::size_t BlockSize = 16 * 1024, std::size_t MaxChunkSize = 1024,
-          std::uint32_t SupportedSizesIncrement = 32, class... Args>
-inline scope<T> make_scope(Args &&...args)
+template <typename T, class... Args> inline scope<T> make_scope(Args &&...args)
 {
     return std::make_unique<T>(std::forward<Args>(args)...);
 }
-
-#endif
 } // namespace kit
