5dc3b452bd13b9bb5cf4d281e1510dd9d9bfc992
Ismael Bueno <ismaelwno@gmail.com>
2024-03-24 02:12:39 +0000

    Remove some template parameters and add other required ones

--- include/kit/memory/ref.hpp
+++ include/kit/memory/ref.hpp
@@ -6,9 +6,7 @@ namespace kit
 {
 template <typename T> using ref = std::shared_ptr<T>;
 
-template <typename T, std::size_t BlockSize = 16 * 1024, std::size_t MaxChunkSize = 1024,
-          std::uint32_t SupportedSizesIncrement = 32, class... Args>
-inline ref<T> make_ref(Args &&...args)
+template <typename T, class... Args> inline ref<T> make_ref(Args &&...args)
 {
     return std::make_shared<T>(std::forward<Args>(args)...);
 }
--- include/kit/memory/scope.hpp
+++ include/kit/memory/scope.hpp
@@ -4,7 +4,7 @@
 
 namespace kit
 {
-template <typename T> using scope = std::unique_ptr<T>;
+template <typename T, typename Deleter = std::default_delete<T>> using scope = std::unique_ptr<T, Deleter>;
 
 template <typename T, class... Args> inline scope<T> make_scope(Args &&...args)
 {
