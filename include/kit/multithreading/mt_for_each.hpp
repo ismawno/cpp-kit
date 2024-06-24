8664a9225f3ac87207dbf7c744049ad1b557287c
Ismael Bueno <ismaelwno@gmail.com>
2024-06-24 23:37:45 +0200

    It is now possible to pass the splits of a parallelized for loop

--- include/kit/multithreading/mt_for_each.hpp
+++ include/kit/multithreading/mt_for_each.hpp
@@ -15,26 +15,50 @@ concept ValidContainer = RandomAccessContainer<T> && requires(T a) {
     } -> std::convertible_to<std::size_t>;
 };
 
-template <std::size_t ThreadCount, ValidContainer C, typename F> void for_each(C &container, F fun)
+template <std::size_t ThreadCount, ValidContainer C, typename F> struct type_helper
 {
-    using It = decltype(std::declval<C>().begin());
-
-    const auto worker = [](It it1, It it2, F fun, const std::size_t thread_idx) {
+    using iterator_t = decltype(std::declval<C>().begin());
+    static inline const auto worker = [](iterator_t it1, iterator_t it2, F fun, const std::size_t thread_idx) {
         for (auto it = it1; it != it2; ++it)
             fun(thread_idx, *it);
     };
 
-    static thread_pool<decltype(worker), It, It, F, std::size_t> pool{ThreadCount};
+    using worker_t = decltype(worker);
+    static inline thread_pool<worker_t, iterator_t, iterator_t, F, std::size_t> pool{ThreadCount};
+};
 
+// consider having thread count as runtime parameter
+template <std::size_t ThreadCount, ValidContainer C, typename F> void for_each(C &container, F fun)
+{
+    auto &pool = type_helper<ThreadCount, C, F>::pool;
     const std::size_t size = container.size();
-    for (std::size_t i = 0; i < pool.size(); i++)
+
+    for (std::size_t i = 0; i < ThreadCount; i++)
     {
-        const std::size_t start = i * size / pool.size();
-        const std::size_t end = (i + 1) * size / pool.size();
+        const std::size_t start = i * size / ThreadCount;
+        const std::size_t end = (i + 1) * size / ThreadCount;
         KIT_ASSERT_ERROR(end <= size, "Partition exceeds vector size! start: {0}, end: {1}, size: {2}", start, end,
                          size)
-        pool.submit(worker, container.begin() + start, container.begin() + end, fun, i);
+        pool.submit(type_helper<ThreadCount, C, F>::worker, container.begin() + start, container.begin() + end, fun, i);
     }
     pool.await_pending();
 }
+
+template <std::size_t ThreadCount, ValidContainer C, typename F>
+void for_each(C &container, F fun, const std::array<std::size_t, ThreadCount - 1> &splits)
+{
+    auto &pool = type_helper<ThreadCount, C, F>::pool;
+    const std::size_t size = container.size();
+
+    for (std::size_t i = 0; i < ThreadCount; i++)
+    {
+        const std::size_t start = i == 0 ? 0 : splits[i - 1];
+        const std::size_t end = i == (ThreadCount - 1) ? size : splits[i];
+        KIT_ASSERT_ERROR(end <= size, "Partition exceeds vector size! start: {0}, end: {1}, size: {2}", start, end,
+                         size)
+        pool.submit(type_helper<ThreadCount, C, F>::worker, container.begin() + start, container.begin() + end, fun, i);
+    }
+    pool.await_pending();
+}
+
 } // namespace kit::mt
