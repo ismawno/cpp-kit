b644e1661f83db78ea7ddf877da73df3a1aea4f5
Ismael Bueno <ismaelwno@gmail.com>
2024-06-25 19:37:30 +0100

    Can now specify splits in mt_for_each

--- include/kit/multithreading/mt_for_each.hpp
+++ include/kit/multithreading/mt_for_each.hpp
@@ -28,35 +28,42 @@ template <std::size_t ThreadCount, ValidContainer C, typename F> struct type_hel
 };
 
 // consider having thread count as runtime parameter
-template <std::size_t ThreadCount, ValidContainer C, typename F> void for_each(C &container, F fun)
+template <std::size_t ThreadCount, ValidContainer C, typename F>
+void for_each(C &container, F fun, const std::size_t splits = ThreadCount)
 {
+    KIT_ASSERT_ERROR(splits > 0, "Splits must be greater than 0")
     auto &pool = type_helper<ThreadCount, C, F>::pool;
     const std::size_t size = container.size();
 
-    for (std::size_t i = 0; i < ThreadCount; i++)
+    for (std::size_t i = 0; i < splits; i++)
     {
-        const std::size_t start = i * size / ThreadCount;
-        const std::size_t end = (i + 1) * size / ThreadCount;
+        const std::size_t start = i * size / splits;
+        const std::size_t end = (i + 1) * size / splits;
         KIT_ASSERT_ERROR(end <= size, "Partition exceeds vector size! start: {0}, end: {1}, size: {2}", start, end,
                          size)
-        pool.submit(type_helper<ThreadCount, C, F>::worker, container.begin() + start, container.begin() + end, fun, i);
+        if (end > start)
+            pool.submit(type_helper<ThreadCount, C, F>::worker, container.begin() + start, container.begin() + end, fun,
+                        i);
     }
     pool.await_pending();
 }
 
 template <std::size_t ThreadCount, ValidContainer C, typename F>
-void for_each(C &container, F fun, const std::array<std::size_t, ThreadCount - 1> &splits)
+void for_each(C &container, F fun, const std::vector<std::size_t> &splits)
 {
+    KIT_ASSERT_ERROR(splits.size() > 0, "Splits must be greater than 0")
     auto &pool = type_helper<ThreadCount, C, F>::pool;
     const std::size_t size = container.size();
 
-    for (std::size_t i = 0; i < ThreadCount; i++)
+    for (std::size_t i = 0; i < splits.size(); i++)
     {
         const std::size_t start = i == 0 ? 0 : splits[i - 1];
-        const std::size_t end = i == (ThreadCount - 1) ? size : splits[i];
+        const std::size_t end = i == (splits.size() - 1) ? size : splits[i];
         KIT_ASSERT_ERROR(end <= size, "Partition exceeds vector size! start: {0}, end: {1}, size: {2}", start, end,
                          size)
-        pool.submit(type_helper<ThreadCount, C, F>::worker, container.begin() + start, container.begin() + end, fun, i);
+        if (end > start)
+            pool.submit(type_helper<ThreadCount, C, F>::worker, container.begin() + start, container.begin() + end, fun,
+                        i);
     }
     pool.await_pending();
 }
