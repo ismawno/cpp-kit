75b095f269c0fd402a1a41dc9a3f5ef7da04256e
Ismael Bueno <ismaelwno@gmail.com>
2024-07-04 20:19:30 +0100

    Implement erase in quad tree

--- include/kit/container/quad_tree.hpp
+++ include/kit/container/quad_tree.hpp
@@ -12,8 +12,6 @@
 #include <array>
 #include <stack>
 
-#define KIT_QT_COLLECT_ELEMENTS_COPY
-
 namespace kit
 {
 template <typename T>
@@ -21,6 +19,12 @@ concept QuadTreeElement = requires(T t) {
     {
         t()
     } -> std::convertible_to<geo::aabb2D>;
+    {
+        t == t
+    } -> std::convertible_to<bool>;
+    {
+        std::hash<T>{}(t)
+    } -> std::convertible_to<std::size_t>;
 };
 
 template <QuadTreeElement T, template <typename> class Allocator = block_allocator> class quad_tree
@@ -28,17 +32,24 @@ template <QuadTreeElement T, template <typename> class Allocator = block_allocat
   public:
     struct partition
     {
-#ifdef KIT_QT_COLLECT_ELEMENTS_COPY
         using to_compare_t = std::vector<T>;
-#else
-        using to_compare_t = std::vector<const std::vector<T> *>;
-#endif
-
         const std::vector<T> *elements;
         to_compare_t to_compare;
     };
 
-    struct properties;
+    struct properties
+    {
+        properties(const std::size_t elements_per_quad, const std::uint32_t max_depth, const float min_quad_size)
+            : elements_per_quad(elements_per_quad), max_depth(max_depth), min_quad_size(min_quad_size)
+        {
+        }
+
+        std::size_t elements_per_quad;
+        std::uint32_t max_depth;
+        float min_quad_size;
+        bool force_square_shape = true;
+    };
+
     struct node;
 
     quad_tree(const std::size_t elements_per_quad = 8, std::uint32_t max_depth = 12, const float min_quad_size = 10.f)
@@ -53,14 +64,11 @@ template <QuadTreeElement T, template <typename> class Allocator = block_allocat
     {
         const auto elements = other.collect_elements();
         for (const auto &elem : elements)
-#ifdef KIT_QT_COLLECT_ELEMENTS_COPY
-            insert_and_grow(elem);
-#else
-            for (const auto &e : *elem)
-                insert_and_grow(e);
-#endif
+            insert(elem);
     }
-    quad_tree(quad_tree &&other) : m_props(std::move(other.m_props)), m_root(std::move(other.m_root))
+    quad_tree(quad_tree &&other)
+        : m_props(std::move(other.m_props)), m_root(std::move(other.m_root)),
+          m_element_bounds(std::move(other.m_element_bounds))
     {
     }
 
@@ -72,52 +80,64 @@ template <QuadTreeElement T, template <typename> class Allocator = block_allocat
         m_root = node(m_props.get());
         const auto elements = other.collect_elements();
         for (const auto &elem : elements)
-#ifdef KIT_QT_COLLECT_ELEMENTS_COPY
-            insert_and_grow(elem);
-#else
-            for (const auto &e : *elem)
-                insert_and_grow(e);
-#endif
+            insert(elem);
         return *this;
     }
     quad_tree &operator=(quad_tree &&other)
     {
         m_props = std::move(other.m_props);
         m_root = std::move(other.m_root);
+        m_element_bounds = std::move(other.m_element_bounds);
         return *this;
     }
 
-    bool insert(const T &element)
+    void insert(const T &element)
     {
         KIT_PERF_FUNCTION()
+        KIT_ASSERT_ERROR(!m_element_bounds.contains(element), "Element already exists in the quad tree")
         const geo::aabb2D &bounds = element();
-        if (!geo::intersects(m_root.aabb, bounds))
-            return false;
-        m_root.insert(element);
-        return true;
-    }
-    void insert_and_grow(const T &element)
-    {
-        KIT_PERF_FUNCTION()
-        const geo::aabb2D &bounds = element();
+        m_element_bounds.emplace(element, bounds);
+
         m_root.aabb += bounds;
         m_root.insert(element);
     }
+
+    bool erase(const T &element)
+    {
+        KIT_PERF_FUNCTION()
+        const auto it = m_element_bounds.find(element);
+        if (it == m_element_bounds.end())
+            return false;
+        const bool found = m_root.erase(element, it->second);
+        KIT_ASSERT_ERROR(found, "Present element was not found in the quad tree")
+        m_element_bounds.erase(it);
+        return found;
+    }
+
     void clear()
     {
-        m_root.elements.clear();
-        m_root.partitioned = false;
+        KIT_PERF_FUNCTION()
+        m_element_bounds.clear();
+        m_root.clear();
+    }
+    std::size_t size() const
+    {
+        return m_root.size();
     }
     bool empty() const
     {
-        return m_root.elements.empty() && !m_root.partitioned;
+        return m_root.empty();
+    }
+    bool checksum() const
+    {
+        return m_element_bounds.size() == m_root.size() && m_element_bounds.empty() == empty();
     }
 
     std::vector<partition> collect_partitions() const
     {
         KIT_PERF_FUNCTION()
         std::vector<partition> partitions;
-        partitions.reserve(32);
+        partitions.reserve(8);
         m_root.collect_partitions(partitions);
         return partitions;
     }
@@ -130,13 +150,27 @@ template <QuadTreeElement T, template <typename> class Allocator = block_allocat
         return elements;
     }
 
+    void rebuild()
+    {
+        const auto elements = collect_elements();
+        clear();
+        for (const auto &elem : elements)
+            insert(elem);
+    }
+
     const geo::aabb2D &bounds() const
     {
         return m_root.aabb;
     }
     void bounds(const geo::aabb2D &bounds)
     {
+        KIT_ASSERT_ERROR(empty(), "Cannot change bounds of a non-empty quad tree")
         m_root.aabb = bounds;
+    }
+    void rebound_and_rebuild(const geo::aabb2D &bounds)
+    {
+        m_root.aabb = bounds;
+        rebuild();
     }
 
     const node &root() const
@@ -147,9 +181,10 @@ template <QuadTreeElement T, template <typename> class Allocator = block_allocat
     {
         return *m_props;
     }
-    properties &props()
+    void props(const properties &props)
     {
-        return *m_props;
+        *m_props = props;
+        rebuild();
     }
 
     struct node
@@ -166,29 +201,69 @@ template <QuadTreeElement T, template <typename> class Allocator = block_allocat
         geo::aabb2D aabb;
 
         std::uint32_t depth = 0;
-        bool partitioned = false;
+        std::size_t children_size = 0;
+
+        bool empty() const
+        {
+            return elements.empty() && children_size == 0;
+        }
+        bool partitioned() const
+        {
+            return children_size != 0;
+        }
+        std::size_t size() const
+        {
+            return elements.size() + children_size;
+        }
+        void clear()
+        {
+            elements.clear();
+            children_size = 0;
+            if (partitioned())
+                for (node *child : children)
+                    child->clear();
+        }
 
         void insert(const T &element)
         {
             KIT_ASSERT_ERROR(geo::intersects(aabb, element()), "Element is not within the bounds of the quad")
-            if (!partitioned && elements.size() >= props->elements_per_quad && can_subdivide())
+            if (!partitioned() && elements.size() >= props->elements_per_quad && can_subdivide())
                 subdivide();
-            if (!partitioned || !try_insert_into_children(element))
+            if (!partitioned() || !try_insert_into_children(element))
                 elements.push_back(element);
+        }
+
+        bool erase(const T &element, const geo::aabb2D &ebounds)
+        {
+            KIT_ASSERT_ERROR(geo::intersects(aabb, ebounds), "Element is not within the bounds of the quad")
+            for (auto it = elements.begin(); it != elements.end(); ++it)
+                if (*it == element)
+                {
+                    elements.erase(it);
+                    return true;
+                }
+            if (!partitioned())
+                return false;
+            for (node *child : children)
+                if (geo::intersects(child->aabb, ebounds) && child->erase(element, ebounds))
+                {
+                    children_size--;
+                    return true;
+                }
+            return false;
         }
 
         void subdivide()
         {
-            partitioned = true;
             if (!children[0])
                 for (std::size_t i = 0; i < 4; ++i)
-                    children[i] = props->m_allocator.create(props);
+                    children[i] = s_allocator.create(props);
 
             for (node *c : children)
             {
                 c->depth = depth + 1;
-                c->partitioned = false;
                 c->elements.clear();
+                c->children_size = 0;
             }
 
             const glm::vec2 &mm = aabb.min;
@@ -224,6 +299,7 @@ template <QuadTreeElement T, template <typename> class Allocator = block_allocat
             if (multiple)
                 return false;
             chosen->insert(element);
+            children_size++;
             return true;
         }
 
@@ -246,7 +322,7 @@ template <QuadTreeElement T, template <typename> class Allocator = block_allocat
                 p.to_compare.reserve(8);
             }
 
-            if (partitioned)
+            if (partitioned())
                 for (node *child : children)
                 {
                     if (!elements.empty())
@@ -257,44 +333,18 @@ template <QuadTreeElement T, template <typename> class Allocator = block_allocat
 
         void collect_elements(partition::to_compare_t &to_insert) const
         {
-#ifdef KIT_QT_COLLECT_ELEMENTS_COPY
             to_insert.insert(to_insert.end(), elements.begin(), elements.end());
-#else
-            if (!elements.empty())
-                to_insert.push_back(&elements);
-#endif
-            if (partitioned)
+            if (partitioned())
                 for (node *child : children)
                     child->collect_elements(to_insert);
         }
     };
 
-    struct properties
-    {
-        properties(const std::size_t elements_per_quad, const std::uint32_t max_depth, const float min_quad_size)
-            : elements_per_quad(elements_per_quad), max_depth(max_depth), min_quad_size(min_quad_size)
-        {
-        }
-
-        std::size_t elements_per_quad;
-        std::uint32_t max_depth;
-        float min_quad_size;
-
-      private:
-        Allocator<node> m_allocator;
-
-        properties(const properties &other) = default;
-        properties(properties &&other) = default;
-
-        properties &operator=(const properties &other) = default;
-        properties &operator=(properties &&other) = default;
-
-        friend struct node;
-        friend class quad_tree;
-    };
-
   private:
     kit::scope<properties> m_props;
     node m_root;
+    std::unordered_map<T, geo::aabb2D> m_element_bounds;
+
+    static inline Allocator<node> s_allocator{};
 };
 } // namespace kit
--- include/kit/utility/utils.hpp
+++ include/kit/utility/utils.hpp
@@ -12,22 +12,38 @@ bool approximately(float a, float b);
 bool approaches_zero(float x);
 float cross2D(const glm::vec2 &v1, const glm::vec2 &v2);
 
-template <typename Derived, template <typename...> typename SmartPtr, typename Base>
-    requires DerivedFrom<Derived, Base>
-const Derived *const_get_casted_raw_ptr(const SmartPtr<Base> &ptr)
+template <typename T, typename Tuple> struct tuple_has_type;
+
+template <typename T, typename Head, typename... Tail>
+struct tuple_has_type<T, std::tuple<Head, Tail...>>
+    : std::disjunction<std::is_same<T, Head>, tuple_has_type<T, std::tuple<Tail...>>>
 {
-    if constexpr (std::is_same_v<Base, Derived>)
-        return ptr.get();
-    else
-        return dynamic_cast<const Derived *>(ptr.get());
-}
+};
+
+template <typename T> struct tuple_has_type<T, std::tuple<>> : std::false_type
+{
+};
+
+template <typename T, typename Tuple> inline constexpr bool tuple_has_type_v = tuple_has_type<T, Tuple>::value;
 
 template <typename Derived, template <typename...> typename SmartPtr, typename Base>
     requires DerivedFrom<Derived, Base>
 Derived *get_casted_raw_ptr(const SmartPtr<Base> &ptr)
 {
-    if constexpr (std::is_same_v<Base, Derived>)
+    if constexpr (std::is_same_v<Base, std::remove_const_t<Derived>>)
         return ptr.get();
+    else
+        return dynamic_cast<Derived *>(ptr.get());
+}
+
+template <typename Derived, template <typename...> typename SmartPtr, typename Base, class... TupleArgs>
+    requires DerivedFrom<Derived, Base>
+Derived *get_casted_raw_ptr(const SmartPtr<Base> &ptr, const std::tuple<TupleArgs *...> &known)
+{
+    if constexpr (std::is_same_v<Base, std::remove_const_t<Derived>>)
+        return ptr.get();
+    else if constexpr (tuple_has_type_v<Derived *, std::tuple<TupleArgs *...>>)
+        return std::get<Derived *>(known);
     else
         return dynamic_cast<Derived *>(ptr.get());
 }
