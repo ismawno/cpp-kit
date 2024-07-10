37bc426a9ce369230b79c3b994b347cea2c11427
Ismael Bueno <ismaelwno@gmail.com>
2024-07-10 03:11:23 +0200

    Weird annoying fix bc msvc cant be a decent compiler

--- include/kit/multithreading/mt_for_each.hpp
+++ include/kit/multithreading/mt_for_each.hpp
@@ -29,20 +29,26 @@ template <typename C, typename F, class... Args> struct type_helper
 {
     using iterator_t = decltype(std::declval<C>().begin());
     using fun_ret_t = std::invoke_result_t<F, std::size_t, typename C::value_type, Args...>;
+    
+    // doing this bc shitty msvc wont discard the right fucking if constexpr branch
+    static void wimpl(iterator_t it1, iterator_t it2, F fun, const std::size_t workload_index, std::true_type, Args &&...args) 
+    {
+        for (auto it = it1; it != it2; ++it)
+            fun(workload_index, *it, std::forward<Args>(args)...);
+    }
+    static auto wimpl(iterator_t it1, iterator_t it2, F fun, const std::size_t workload_index, std::false_type, Args &&...args) 
+    {
+        std::vector<fun_ret_t> ret;
+        ret.reserve(std::distance(it1, it2));
+        for (auto it = it1; it != it2; ++it)
+            ret.push_back(fun(workload_index, *it, std::forward<Args>(args)...));
+        return ret;
+    }
 
     static inline const auto worker = [](iterator_t it1, iterator_t it2, F fun, const std::size_t workload_index,
-                                         Args &&...args) {
-        if constexpr (std::is_same_v<fun_ret_t, void>)
-            for (auto it = it1; it != it2; ++it)
-                fun(workload_index, *it, std::forward<Args>(args)...);
-        else
-        {
-            std::vector<fun_ret_t> ret;
-            ret.reserve(std::distance(it1, it2));
-            for (auto it = it1; it != it2; ++it)
-                ret.push_back(fun(workload_index, *it, std::forward<Args>(args)...));
-            return ret;
-        }
+                                         Args &&...args) 
+    {
+        return wimpl(it1, it2, fun, workload_index, std::is_same<fun_ret_t, void>{}, std::forward<Args>(args)...);
     };
 };
 
